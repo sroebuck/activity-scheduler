f7d643f474b55d1900b7b3cfa29feb88f7e78a00
Stuart Roebuck <stuart.roebuck@gmail.com>
2015-02-04 17:42:05 +0000

    Added activities and groups to table display.

--- react/dist/myapp.jsx
+++ react/dist/myapp.jsx
@@ -26,10 +26,33 @@ var ScheduleTableElement = React.createClass({
       ),
       React.createElement(
         "table",
-        null,
-        this.state.plans.map(function (plan) {
-          return React.createElement(IndividualTableLine, { key: plan.individual.name, name: plan.individual.name });
-        })
+        { className: "table table-striped table-condensed" },
+        React.createElement(
+          "thead",
+          null,
+          React.createElement(
+            "tr",
+            null,
+            React.createElement(
+              "th",
+              null,
+              "Name"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Group"
+            )
+          )
+        ),
+        React.createElement(
+          "tbody",
+          null,
+          this.state.plans.map(function (plan) {
+            var i = plan.individual;
+            return React.createElement(IndividualTableLine, { key: i.name, name: i.name, group: i.group, ratings: i.ratings, places: plan.places });
+          })
+        )
       )
     );
   }
@@ -38,13 +61,46 @@ var ScheduleTableElement = React.createClass({
 var IndividualTableLine = React.createClass({
   displayName: "IndividualTableLine",
   render: function () {
+    var _props = this.props;
+    var places = new Map(_props.places.map(function (place) {
+      return [place.slot, place.activity];
+    }));
+    var a1 = places.get("11am-12noon");
+    var a2 = places.get("12noon-1pm");
+    var a3 = places.get("2pm-3pm");
+    var a4 = places.get("3pm-4pm");
     return React.createElement(
       "tr",
       null,
       React.createElement(
         "td",
         null,
-        this.props.name
+        _props.name
+      ),
+      React.createElement(
+        "td",
+        null,
+        _props.group
+      ),
+      React.createElement(
+        "td",
+        null,
+        a1
+      ),
+      React.createElement(
+        "td",
+        null,
+        a2
+      ),
+      React.createElement(
+        "td",
+        null,
+        a3
+      ),
+      React.createElement(
+        "td",
+        null,
+        a4
       )
     );
   }
--- react/js/myapp.jsx
+++ react/js/myapp.jsx
@@ -11,12 +11,16 @@ let ScheduleTableElement = React.createClass({
     return (
       <div>
         <p>There were {this.state.plans.length} individuals plans found.</p>
-        <table>
-        {
-          this.state.plans.map( plan =>
-            <IndividualTableLine key={plan.individual.name} name={plan.individual.name} />
-          )
-        }
+        <table className="table table-striped table-condensed">
+          <thead>
+            <tr><th>Name</th><th>Group</th></tr>
+          </thead>
+          <tbody>{
+            this.state.plans.map( plan => {
+              let i = plan.individual;
+              return <IndividualTableLine key={i.name} name={i.name} group={i.group} ratings={i.ratings} places={plan.places} />;
+            })
+          }</tbody>
         </table>
       </div>
     );
@@ -25,7 +29,22 @@ let ScheduleTableElement = React.createClass({
 
 let IndividualTableLine = React.createClass({
   render: function () {
-    return <tr><td>{this.props.name}</td></tr>;
+    let _props = this.props;
+    let places = new Map(_props.places.map( place => [place.slot, place.activity]));
+    let a1 = places.get("11am-12noon");
+    let a2 = places.get("12noon-1pm");
+    let a3 = places.get("2pm-3pm");
+    let a4 = places.get("3pm-4pm");
+    return (
+      <tr>
+        <td>{_props.name}</td>
+        <td>{_props.group}</td>
+        <td>{a1}</td>
+        <td>{a2}</td>
+        <td>{a3}</td>
+        <td>{a4}</td>
+      </tr>
+    );
   }
 });
 
